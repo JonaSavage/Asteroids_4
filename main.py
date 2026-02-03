c550322c608dcac8f1dc8c152f32f2f6ec16715e
JonaSavage <jonathan.stickle@gmail.com>
2026-02-03 12:34:07 -0800

    Test one more time

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import pygame
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print("This is to test if I have to use my PAT again.")
+    print("One more time.")
 
 
 if __name__ == "__main__":
