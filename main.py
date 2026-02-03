1eaa4d7abe744c63669250fce2c4d3c9d90006de
JonaSavage <jonathan.stickle@gmail.com>
2026-02-03 12:37:50 -0800

    Base of project is working now

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 # Run main.py: uv run main.py
 
 import pygame
+from constants import *
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print("This is to test if I have to use my PAT again.")
-    print("One more time.")
+    print("Starting Asteroids...")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
