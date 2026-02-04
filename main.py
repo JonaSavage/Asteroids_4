a5da3eeac60d07b95873df8998e56b2246f232aa
JonaSavage <jonathan.stickle@gmail.com>
2026-02-04 14:00:56 -0800

    Game window opens and closes correctly.

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 
 import pygame
 from constants import *
+from logger import log_state
 
 def main():
-    print("Starting Asteroids...")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen Height: {SCREEN_HEIGHT}")
-    print()
+    pygame.init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        log_state()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+    
 
 
 if __name__ == "__main__":
