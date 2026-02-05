bb1c50e1be2ebb3308cb400c33580ee3089ca157
JonaSavage <jonathan.stickle@gmail.com>
2026-02-04 17:14:13 -0800

    Chapter 3: Player done.

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    
     
     while True:
         log_state()
@@ -20,7 +33,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        updatable.update(dt)
+        for i in drawable:
+            i.draw(screen) 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
     
 
 
